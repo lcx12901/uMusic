3dc2a46654d9634820ebe7e86c346ba31a48593f
lcx12901 <1367372276@qq.com>
2021-08-01 23:31:57 +0800

    订正

--- index.js
+++ index.js
@@ -2,18 +2,6 @@ var express = require('express');
 var app = express();
 const match = require('@revincx/unblockneteasemusic');
 const PROT = process.env_PROT || 8080
-app.all('*',function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-
-	if (req.method == 'OPTIONS') {
-		res.send(200);
-	}
-	else {
-		next();
-	}
-});
 
 app.get('/', function(res, rep) {
 	rep.send('index.html');
